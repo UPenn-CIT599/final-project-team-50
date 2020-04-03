02b8bf82f1ba0fa0a7a4411244a7b7ccaf899286
yaoyao-1113 <yaoyao19901113@gmail.com>
2020-04-03 16:22:25 -0400

    Created some method

--- src/simulation.java
+++ src/simulation.java
@@ -1,5 +1,6 @@
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * @author yao16
@@ -20,30 +21,35 @@ public class simulation {
 	 * This method will take the location of one exit and generate a 2D array
 	 * to store the distance between this exit to all the available location (not a wall) on a floor map. 
 	 */
-	public int[][] getDistanceWithWall(exit e){
-		int [][] distanceWithWall = new int [f.getSize()][f.getSize()];
-		
+	public int[][] getDistanceWithWall(exit e) {
+		int[][] distanceWithWall = new int[f.getSize()][f.getSize()];
+
 		//implement the method;
 		
 		return distanceWithWall;
 	}
 	/**
-	 * @param location of a person
-	 * This method will take the location of one person and generate an integer
-	 * represent the shortest distance to a closest exit 
+	 * @param A person
+	 * This method will take one person and return the closest exit he/she should go
 	 */
-	public int getShortestDistance(int r, int c) {
-		ArrayList<Integer> distancesToExit = new ArrayList<Integer>();
+	public exit closestExit(person p) {
+		exit closestExit = new exit();	
+		HashMap<exit, Integer> map = new HashMap<exit,Integer>(); //to store at the position this person standing, the pair of exit-distance.
 
-		for (exit e : exits) {
-			distancesToExit.add(getDistanceWithWall(e)[r][c]);
-		}
+		//implement the method find the exit with the shortest distance
 		
-		distancesToExit.sort(null);
-		return distancesToExit.get(0);
+		return closestExit;
 	}
-	
-	
-	
+	/**
+	 * @param A person
+	 * This method will take one person and return the distance between this person and the exit he/she is going
+	 */
+	public person[] movePriority(person[] people) { 
+		person[] sortedPeople = new person[this.people.length];
+		
+		//implement the method
+		
+		return sortedPeople;
+	}
 	
 }
