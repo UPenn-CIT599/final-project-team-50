c146fe91d257dd914991fc32fcbbbba7996193c5
Chi Zhang <chizhang@Chis-MBP-2.fios-router.home>
2020-04-02 23:42:49 -0400

    FloorPlan and UserInput are updated
    s#

---
+++ src/UserInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class UserInput {
+	
+	public void userInput() {
+		
+		System.out.println("Please enter the number of people");
+		Scanner people = new Scanner(System.in);
+		int NumofPeople = people.nextInt();
+		
+		System.out.println("Please entern the number of exits");
+		Scanner exits = new Scanner(System.in);
+		int NumofExits = exits.nextInt();
+		
+		
+		
+	}
+
+
+
+
+}
--- src/floorPlan.java
+++ src/floorPlan.java
@@ -1,39 +1,95 @@
 import java.util.ArrayList;
 
-public class floorPlan {
-	private int size;	// # of rows and columns in table
-	private boolean[][]	walls;	// Grid indicating whether a wall is there or not
-	private ArrayList<exit>	exits;	// List of available building exits
+public class FloorPlan {
 	
-	public floorPlan(int rw) {
-		size = rw;
-		exits = new ArrayList<exit>();
-		walls = new boolean[size][size];
+	//size = 3n+1 (n >= 10)
+	private int size;
+	private boolean[][] walls;
+	private ArrayList<People> people;
+	private ArrayList<Exit> exits;
+	
+	
+	public void floorPlan(int k) {
+		this.size = k;
+		this.exits = new ArrayList<Exit>();
+		this.walls = new boolean [size][size];
+		
+		//core
+		//r = 1/3 * size
+		for(int c = 1/3 * size; c <= 2/3 * size; c++ ) {
+			walls[1/3 * size][c] = true;
+		}
+		
+		//r = 2/3 * size
+		for(int c = 1/3 * size; c <= 2/3 * size; c++ ) {
+			walls[2/3 * size][c] = true;
+		}
+		
+		//c = 1/3 * size
+		for(int r = 1/3 * size; r <= 2/3 * size; r++ ) {
+			if(r == 1/2 * size) {
+				continue;
+			}
+			walls[r][1/3 * size] = true;
+		}
+		
+		//c = 2/3 * size
+		for(int r = 1/3 * size; r <= 2/3 * size; r++) {
+			if(r == 1/2 * size) {
+				continue;
+			}
+			walls[r][2/3 * size] = true;
+		}
+		
+		//perimeter
+		for(int c=0; c < size; c++) {
+			for(int r=0; r < size; r++) {
+				if ((r==0)||(r==size-1)||(c==0)||(c==size-1)) {
+					walls[r][c] = true;
+				}
+				else {
+					walls[r][c] = false;
+				}
+			}
+		}
+		
 	}
-
+	
 	public int getSize() {
 		return size;
-	}
-
-	public void setSize(int size) {
-		this.size = size;
 	}
 
 	public boolean[][] getWalls() {
 		return walls;
 	}
 
-	public void setWalls(boolean[][] walls) {
-		this.walls = walls;
+	public ArrayList<People> getPeople() {
+		return people;
 	}
 
-	public ArrayList<exit> getExits() {
+	public ArrayList<Exit> getExits() {
 		return exits;
 	}
 
-	public void setExits(ArrayList<exit> exits) {
+	
+	public boolean valid(int r, int c) {
+		return (r>=0)&&(r<size)&&(c>=0)&&(c<size);
+	}
+
+	public void setPeople(ArrayList<People> people) {
+		this.people = people;
+	}
+
+	public void setExits(ArrayList<Exit> exits) {
 		this.exits = exits;
 	}
 	
+	public void addExit(Exit exit){
+		exits.add(exit);
+	}
 	
+	public void addPeople(People ppl) {
+		people.add(ppl);
+	}
+
 }
