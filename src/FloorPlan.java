aa86356ff1eba62def74d99a8e100bde723557f3
Chi Zhang <chizhang@Chis-MBP-2.fios-router.home>
2020-04-04 14:57:23 -0400

    Please see the updated floor plan java file

--- src/FloorPlan.java
+++ src/FloorPlan.java
@@ -1,28 +1,32 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class FloorPlan {
 	
+	public int maxExit = 8;
+	
 	//size = 3n+1 (n >= 10)
 	private int size;
-	private boolean[][] walls;
-	private ArrayList<People> people;
-	private ArrayList<Exit> exits;
+	private int[][] floorPlan;
+	private Exit exit;
 	
 	
-	public void floorPlan(int k) {
+	public void floorPlan(int k, Exit exits) {
 		this.size = k;
-		this.exits = new ArrayList<Exit>();
-		this.walls = new boolean [size][size];
+		this.exit = exits;
+		this.floorPlan = new int [size][size];
 		
 		//core
 		//r = 1/3 * size
 		for(int c = 1/3 * size; c <= 2/3 * size; c++ ) {
-			walls[1/3 * size][c] = true;
+			floorPlan[1/3 * size][c] = 1;
 		}
 		
 		//r = 2/3 * size
 		for(int c = 1/3 * size; c <= 2/3 * size; c++ ) {
-			walls[2/3 * size][c] = true;
+			floorPlan[2/3 * size][c] = 1;
 		}
 		
 		//c = 1/3 * size
@@ -30,7 +34,7 @@ public class FloorPlan {
 			if(r == 1/2 * size) {
 				continue;
 			}
-			walls[r][1/3 * size] = true;
+			floorPlan[r][1/3 * size] = 1;
 		}
 		
 		//c = 2/3 * size
@@ -38,19 +42,26 @@ public class FloorPlan {
 			if(r == 1/2 * size) {
 				continue;
 			}
-			walls[r][2/3 * size] = true;
+			floorPlan[r][2/3 * size] = 1;
 		}
 		
 		//perimeter
 		for(int c=0; c < size; c++) {
 			for(int r=0; r < size; r++) {
 				if ((r==0)||(r==size-1)||(c==0)||(c==size-1)) {
-					walls[r][c] = true;
+					floorPlan[r][c] = 1;
 				}
 				else {
-					walls[r][c] = false;
+					floorPlan[r][c] = 0;
 				}
 			}
+		}
+		
+		ArrayList<int[]> exitLocations = exit.getExits();
+		for(int[] point: exitLocations) {
+			int r = point[0];
+			int c = point[1];
+			floorPlan[r][c] = 2;
 		}
 		
 	}
@@ -59,37 +70,55 @@ public class FloorPlan {
 		return size;
 	}
 
-	public boolean[][] getWalls() {
-		return walls;
+	public int [][] getWalls() {
+		return floorPlan;
 	}
-
-	public ArrayList<People> getPeople() {
-		return people;
-	}
-
-	public ArrayList<Exit> getExits() {
-		return exits;
-	}
-
 	
-	public boolean valid(int r, int c) {
+	public Exit getExit() {
+		return exit;
+	}
+
+	public void setExit(Exit exit) {
+		this.exit = exit;
+	}
+
+	public boolean inbound(int r, int c) {
 		return (r>=0)&&(r<size)&&(c>=0)&&(c<size);
 	}
 
-	public void setPeople(ArrayList<People> people) {
-		this.people = people;
-	}
-
-	public void setExits(ArrayList<Exit> exits) {
-		this.exits = exits;
-	}
-	
-	public void addExit(Exit exit){
-		exits.add(exit);
-	}
-	
-	public void addPeople(People ppl) {
-		people.add(ppl);
-	}
+    public boolean hasPeopleAt(int r, int c) {
+    	return floorPlan[r][c] == 3;
+    }
+    
+    public int getMaxPeople () {
+    	int maxNumPeople = 0;
+    	for(int i = 0; i < size; i++) {
+    		for(int j = 0; j < size; j++) {
+    			if(floorPlan[i][j] == 0) {
+    				maxNumPeople++;
+    			}
+    		}
+    	}
+    	return maxNumPeople;
+    	
+    }
+    
+    public boolean locatePeople(int numPeople, int size) {
+    	int max = getMaxPeople();
+    	if(numPeople > max)
+    		return false;
+    	int total = size * size;
+    	Random random = new Random();
+    	while(numPeople > 0) {
+    		int next = random.nextInt(total);
+    		int r = next / size;
+    		int c = next % size;
+    		if(floorPlan[r][c] != 0) {
+    			floorPlan[r][c] = 3;
+    			numPeople--;
+    		}
+    	}
+    	return true;
+    }
 
 }
