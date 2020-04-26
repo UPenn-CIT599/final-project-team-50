e7f054f2f7284bae043c6989e0439cfd026659c5
Chi Zhang <chizhang@Chis-MBP-2.fios-router.home>
2020-04-26 16:11:02 -0400

    update for GUI

--- src/SaftyMapApp.java
+++ src/SaftyMapApp.java
@@ -9,136 +9,145 @@ import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import java.awt.FlowLayout;
+
 import java.awt.Font;
 import javax.swing.JComboBox;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
 
-public class SaftyMapApp extends JFrame {
-
-	private JFrame frame;
-	private JTextField floorSize;
-	private JTextField numberOfPeople;
-	private JComboBox numberOfExit;
-	private ActionListener listener;
-	private JPanel panel;
-	
-	private Simulation s;
-//	private UserInput input;
-
-
-	/**
-	 * Create the application.
-	 */
-	public SaftyMapApp() {
-		listener = new ExtractInfoListener();
-		initialize();
-	}
-	
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setLayout(null);
+	public class SaftyMapApp extends JFrame {
+		private JLabel lblNewLabel;
+		private JLabel lblNewLabel_1;
+		private JLabel lblNewLabel_2;
+		private JTextField floorSize;
+		private JTextField numberOfPeople;
+		private JComboBox numberOfExit;
+		private ActionListener listener;
+		private JPanel panel;
 		
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(650, 600);
-		
-		JLabel lblNewLabel = new JLabel("Please enter your floor size:");
-		lblNewLabel.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 11));
-		lblNewLabel.setBounds(201, 105, 257, 21);
-		frame.getContentPane().add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("How many people on your floor?");
-		lblNewLabel_1.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 11));
-		lblNewLabel_1.setBounds(201, 180, 257, 21);
-		frame.getContentPane().add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("How many exits on the floor?");
-		lblNewLabel_2.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 11));
-		lblNewLabel_2.setBounds(201, 256, 257, 21);
-		frame.getContentPane().add(lblNewLabel_2);
-	
+		private Simulation simulation;
+//		private UserInput input;
 
-		createButton();
-		createTexFieldForSize();
-		createTexFieldForPeople();
-		createTexFieldForSize();
-		createJComboBox();
 
-	}
-
-	class ExtractInfoListener implements ActionListener {
-
-		public void actionPerformed(ActionEvent e) {
-			extractInput();
+		/**
+		 * Create the application.
+		 */
+		public SaftyMapApp() {
+			listener = new ExtractInfoListener();
+			initialize();
 		}
-	}
-
-	public void extractInput() {
-		int exitAmount = Integer.parseInt(numberOfExit.getSelectedItem().toString()); // take the value user input and store in exitAmount.	
-		int size = Integer.parseInt(floorSize.getText());
-		int peopleAmount = Integer.parseInt(numberOfPeople.getText());
 		
-//		Planner p = new Planner (size, exitAmount, peopleAmount);
-//		FloorPlan res = s.provideOptimalFloorPlan(p);
-//		
-//		new OptimalFloorDesign(res).setVisible(true);
-		
-	}
+		/**
+		 * Initialize the contents of the frame.
+		 */
+		private void initialize() {
+			getContentPane().setLayout(null);
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+			setSize(650, 600);
+			setVisible(true);
 
-	
-
-	private void createTexFieldForSize() {
-		floorSize = new JTextField();
-		floorSize.setBounds(201, 126, 257, 35);
-		frame.getContentPane().add(floorSize);
-		floorSize.setColumns(10);
-//		floorSize.addActionListener(listener);
+			panel = new JPanel();
+			panel.setSize(650, 600);
+			getContentPane().add(panel);
 		
-	}
 
-	private void createTexFieldForPeople() {
-		numberOfPeople = new JTextField();
-		numberOfPeople.setBounds(201, 199, 257, 35);
-		numberOfPeople.setColumns(10);
-		frame.getContentPane().add(numberOfPeople);
-		numberOfPeople.addActionListener(listener);
-	}
+			createButton();
+			createTexFieldForSize();
+			createTexFieldForPeople();
+			createTexFieldForSize();
+			createJComboBox();
+
+		}
+
+		class ExtractInfoListener implements ActionListener {
+
+			public void actionPerformed(ActionEvent e) {
+				extractInput();
+			}
+		}
+
+		public void extractInput() {
+			int exitAmount = Integer.parseInt(numberOfExit.getSelectedItem().toString()); // take the value user input and store in exitAmount.	
+			int size = Integer.parseInt(floorSize.getText());
+			int peopleAmount = Integer.parseInt(numberOfPeople.getText());
+			Planner planner = new Planner (size, exitAmount, peopleAmount);
+			Simulation s = new Simulation();
+			s.simulate(planner);
+		}
+
 		
-	private void createJComboBox() {
-		numberOfExit = new JComboBox();
-		numberOfExit.setBounds(201, 277, 257, 35);
-		frame.getContentPane().add(numberOfExit);		
-		numberOfExit.addItem(1);
-		numberOfExit.addItem(2);
-		numberOfExit.addItem(3);
-		numberOfExit.addItem(4);
-		numberOfExit.addItem(5);
-		numberOfExit.addItem(6);
-		numberOfExit.addItem(7);
-		numberOfExit.addItem(8);
-		numberOfExit.setSelectedItem(null);
-		numberOfExit.addActionListener(listener);
-	}
-	
-	private void createButton() {
-		JButton RunButton = new JButton("Run");
-		RunButton.setForeground(new Color(255, 255, 255));
-		RunButton.setFont(new Font("Showcard Gothic", Font.PLAIN, 16));
-		RunButton.setBackground(new Color(51, 153, 0));
-		RunButton.setBounds(251, 411, 155, 43);
-		frame.getContentPane().add(RunButton);
-		RunButton.addActionListener(listener);	
-	}
+		
+
+		private void createTexFieldForSize() {
+//			floorSize.addActionListener(listener);
 			
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		SaftyMapApp frame = new SaftyMapApp();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-	}
+		}
+
+		private void createTexFieldForPeople() {
+		}
+			
+		private void createJComboBox() {
+			
+			lblNewLabel = new JLabel("Please enter your floor size:");
+			panel.add(lblNewLabel);
+			lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			lblNewLabel.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 11));
+			lblNewLabel.setBounds(201, 126, 257, 21);
+			
+			floorSize = new JTextField();
+			floorSize.setBounds(201, 126, 257, 35);
+			panel.add(floorSize);
+			floorSize.setColumns(10);
+			
+			lblNewLabel_1 = new JLabel("How many people on your floor?");
+			lblNewLabel_1.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 11));
+			lblNewLabel_1.setBounds(201, 180, 257, 21);
+			panel.add(lblNewLabel_1);
+			numberOfPeople = new JTextField();
+			numberOfPeople.setBounds(201, 199, 257, 35);
+			numberOfPeople.setColumns(10);
+			panel.add(numberOfPeople);
+			numberOfPeople.addActionListener(listener);
+			
+			lblNewLabel_2 = new JLabel("How many exits on the floor?");
+			lblNewLabel_2.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 11));
+			lblNewLabel_2.setBounds(201, 256, 257, 21);
+			panel.add(lblNewLabel_2);
+			numberOfExit = new JComboBox();
+			numberOfExit.setBounds(201, 277, 257, 35);
+			panel.add(numberOfExit);
+			numberOfExit.addItem(1);
+			numberOfExit.addItem(2);
+			numberOfExit.addItem(3);
+			numberOfExit.addItem(4);
+			numberOfExit.addItem(5);
+			numberOfExit.addItem(6);
+			numberOfExit.addItem(7);
+			numberOfExit.addItem(8);
+			numberOfExit.setSelectedItem(null);
+			JButton RunButton = new JButton("RUN");
+			panel.add(RunButton);
+			RunButton.setBackground(Color.GREEN);
+			RunButton.setBounds(251, 411, 155, 43);
+			RunButton.addActionListener(listener);	
+
+			//numberOfExit.addActionListener(listener);
+		}
+		
+		private void createButton() {
+		}
+				
+		/**
+		 * Launch the application.
+		 */
+		public static void main(String[] args) {
+			SaftyMapApp frame = new SaftyMapApp();
+		}
+	
+
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,148 +1,91 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map.Entry;
 
 public class Simulation {
+	public void simulate(Planner Plan) {
+		//Planner Plan = new Planner();
+		int[][] Floor = Plan.getFloor().getfloorPlan();
+		ArrayList<Person> People = Plan.getPeople();
 
-	public static void main(String[] args) {
-		Simulation simulation = new Simulation(9);
-		Planner planner = new Planner(9, 3, 5);
-		HashMap<Exit, Integer> simulationResults = simulation.runSimulation(planner);
-		int shortestTime = Collections.min(simulationResults.values());
-		
-		FloorPlan floorPlan = simulation.getOptimalFloor(simulation.optimalExitLocation(simulationResults));
+		int timer = 0;
 
-		int[][] Floor = floorPlan.getfloorPlan();
-		
-		System.out.println("The optimal exit locate is below: ");
-		 for (int i = 0; i < Floor.length; ++i) { 
-			 for (int j = 0; j < Floor[i].length; ++j) { 
-				 if (Floor[i][j] == 0) { System.out.print(" "); } 
-				 else {
-					 System.out.print(Floor[i][j]); 
-				 } 
-			 } 
-			 System.out.println(); 
-		 }		
-		 System.out.println("The time for everyone to escape is "+ shortestTime + " mins");
-	}
-	
-	int floorSize;
-	public Simulation(int s) {
-		floorSize = s;
-	}
+		while (Plan.getFloor().hasPeopleOnFloor()) {
+			for (Person p : People) {
+				// index of current location
+				int iCur = p.getCurrentLocation()[0]; // row
+				int jCur = p.getCurrentLocation()[1]; // column
+				int iNext = iCur;
+				int jNext = jCur;
 
-	public HashMap<Exit, Integer> runSimulation(Planner initialPlan) {
+				int[][] distanceGrid = Plan.getDistanceWithWall(Plan.getClosetExit(p));
 
-		HashMap<Exit, Integer> simulationRes = new HashMap<Exit, Integer>();
+				if (p.getDistanceToExit() == 1) {
+					People.remove(new Person(p.getCurrentLocation()));
+					Floor[iCur][jCur] = 0;
+				}
 
-		for (int i = 0; i < 30; i++) {
-			int exitAmount = initialPlan.getExits().getExitLocations().size();		
-			int peopleAmount = initialPlan.getPeople().size();	
-			
-			Planner plans = new Planner(floorSize, exitAmount, peopleAmount);
-			
-			ArrayList<Person> People = plans.getPeople();
-			Exit exitLocation = plans.getExits();
-	
-			FloorPlan floorPlan = plans.getFloor();	
-			int[][] Floor = floorPlan.getfloorPlan();
-			
-//			System.out.println("The initial floor is below: ");
-//			for (int r = 0; r < Floor.length; ++r) {
-//				for (int j = 0; j < Floor[i].length; ++j) {
-//					if (Floor[r][j] == 0) {
-//						System.out.print(" ");
-//					} else {
-//						System.out.print(Floor[r][j]);
-//					}
-//				}
-//				System.out.println();
-//			}
+				else {
+					if (iCur > 0) {
 
-			int timer = 0;
-			while (floorPlan.hasPeopleOnFloor()) {
-				for (Person p : People) {
-					// index of current location
-					int iCur = p.getCurrentLocation()[0]; // row
-					int jCur = p.getCurrentLocation()[1]; // column
-					int iNext = iCur;
-					int jNext = jCur;
-
-					int[][] distanceGrid = plans.getDistanceWithWall(plans.getClosetExit(p));
-
-					if (p.getDistanceToExit() == 1) {
-						
-						People.remove(new Person(p.getCurrentLocation()));
-						floorPlan.getfloorPlan()[iCur][jCur] = 0;
+						int distanceIfGoUp = distanceGrid[iCur - 1][jCur];
+						if (distanceIfGoUp == p.getDistanceToExit() - 1 && Floor[iCur - 1][jCur] == 0) {
+							iNext = iCur - 1;
+						}
 					}
 
-					else {
-						if (iCur > 0) {
-
-							int distanceIfGoUp = distanceGrid[iCur - 1][jCur];
-							if (distanceIfGoUp == p.getDistanceToExit() - 1 && Floor[iCur - 1][jCur] == 0) {
-								iNext = iCur - 1;
-							}
+					if (iCur < Plan.getFloor().getSize() - 1) {
+						int distanceIfGoDown = distanceGrid[iCur + 1][jCur];
+						if (distanceIfGoDown == p.getDistanceToExit() - 1 && Floor[iCur + 1][jCur] == 0) {
+							iNext = iCur + 1;
 						}
+					}
 
-						if (iCur < plans.getFloor().getSize() - 1) {
-							int distanceIfGoDown = distanceGrid[iCur + 1][jCur];
-							if (distanceIfGoDown == p.getDistanceToExit() - 1 && Floor[iCur + 1][jCur] == 0) {
-								iNext = iCur + 1;
-							}
+					if (jCur > 0) {
+						int distanceIfGoLeft = distanceGrid[iCur][jCur - 1];
+						if (distanceIfGoLeft == p.getDistanceToExit() - 1 && Floor[iCur][jCur - 1] == 0) {
+							jNext = jCur - 1;
 						}
+					}
 
-						if (jCur > 0) {
-							int distanceIfGoLeft = distanceGrid[iCur][jCur - 1];
-							if (distanceIfGoLeft == p.getDistanceToExit() - 1 && Floor[iCur][jCur - 1] == 0) {
-								jNext = jCur - 1;
-							}
+					if (jCur < Plan.getFloor().getSize() - 1) {
+						int distanceIfGoRight = distanceGrid[iCur][jCur + 1];
+						if (distanceIfGoRight == p.getDistanceToExit() - 1 && Floor[iCur][jCur + 1] == 0) {
+							jNext = jCur + 1;
 						}
+					}
+					Floor[iCur][jCur] = 0;
+					Floor[iNext][jNext] = 3;
 
-						if (jCur < plans.getFloor().getSize() - 1) {
-							int distanceIfGoRight = distanceGrid[iCur][jCur + 1];
-							if (distanceIfGoRight == p.getDistanceToExit() - 1 && Floor[iCur][jCur + 1] == 0) {
-								jNext = jCur + 1;
-							}
-						}
-						Floor[iCur][jCur] = 0;
-						Floor[iNext][jNext] = 3;
+					iCur = iNext;
+					jCur = jNext;
+					p.setCurrentLocation(iCur, jCur);
+					p.setDistanceToExit(distanceGrid[iCur][jCur]);
 
-						iCur = iNext;
-						jCur = jNext;
-						p.setCurrentLocation(iCur, jCur);
-						p.setDistanceToExit(distanceGrid[iCur][jCur]);
+				}
 
+			}
+			timer++;
+
+			for (int i = 0; i < Floor.length; ++i) {
+				for (int j = 0; j < Floor[i].length; ++j) {
+					if (Floor[i][j] == 0) {
+						System.out.print(" ");
+					} else {
+						System.out.print(Floor[i][j]);
 					}
 				}
-				timer++;
-				
-				
+				System.out.println();
 			}
-			simulationRes.put(exitLocation, timer);
+		}
+		System.out.println("The escape takes " + timer + " mins");
+	}
+
+	/*
+	public static void main(String[] args) {
+		Simulation simulation = new Simulation();
+		//simulation.simulate();
+
+	}*/
+
+}			
 			
-
-		}
-		return simulationRes;
-	}
-
-	private Exit optimalExitLocation(HashMap<Exit, Integer> results) {
-		Exit bestSolution = null;
-		int shortestTime = Collections.min(results.values());
-		for (Entry<Exit, Integer> entry : results.entrySet()) {
-			if (entry.getValue() == shortestTime) {
-				bestSolution = entry.getKey();
-			}
-		}
-		return bestSolution;
-	}
-	
-	
-	public FloorPlan getOptimalFloor (Exit e) {
-		FloorPlan optimalFloorPlan = new FloorPlan(floorSize, e);
-		return optimalFloorPlan;
-	}
-	
-}
+			
