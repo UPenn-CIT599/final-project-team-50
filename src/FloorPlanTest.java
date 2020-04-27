8e8013b71104a0eaf67e424d60aa7f754b891837
chizh0129 <62856256+chizh0129@users.noreply.github.com>
2020-04-26 21:42:58 -0400

    Add floorplan test

--- src/FloorPlanTest.java
+++ src/FloorPlanTest.java
@@ -1,29 +1,72 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-
 import org.junit.jupiter.api.Test;
 
 class FloorPlanTest {
 	@Test
-	public void testLocatePeople () {
+	public void testLocatePeopleFailed () {
 		int k = 12;
 		Exit exits = new Exit();
 		
 		FloorPlan fp = new FloorPlan(k, exits);
 		
-		int [][] res =  fp.getfloorPlan();
-		for (int i = 0; i < res.length; ++i) {
-			for (int j = 0; j < res[i].length; ++j) {
-				System.out.print(res[i][j]);
-			}
-			System.out.println();
-		}
-		
 		
 		boolean result1 = fp.locatePeople(54);
 		assertTrue(result1);
-		boolean result2 = fp.locatePeople(55);
+	}
+	
+	
+	@Test
+	public void testLocatePeopleSucess () {
+		int k = 12;
+		Exit exits = new Exit();
+		
+		FloorPlan fp = new FloorPlan(k, exits);
+		
+		boolean result2 = fp.locatePeople(87);
 		assertFalse(result2);
 	}
-
+	
+	@Test
+	public void testhasPeopleOnFloor () {
+		int k = 12;
+		Exit exits = new Exit();
+		FloorPlan fp = new FloorPlan(k, exits);
+		fp.locatePeople(3);
+		boolean res = fp.hasPeopleOnFloor();
+		
+		assertTrue(res);
+	}
+	
+	@Test
+	public void testInbound() {
+		int k = 12;
+		Exit exits = new Exit();
+		FloorPlan fp = new FloorPlan(k, exits);
+		
+		boolean result1 = fp.inbound(6,6);
+		assertTrue(result1);
+		boolean result2 = fp.inbound(20,11);
+		assertFalse(result2);
+	}
+	
+	@Test
+	public void testGetMaxPeople() {
+		int k = 12;
+		Exit exits = new Exit();
+		FloorPlan fp = new FloorPlan(k, exits);
+		
+		assertEquals(fp.getMaxPeople(),86);
+	}
+	
+	@Test
+	public void testGetFloorPlan() {
+		int k = 12;
+		Exit exits = new Exit();
+		FloorPlan fp = new FloorPlan(k, exits);
+		int[][] floorPlan = fp.getfloorPlan();
+		
+		assertEquals(1, floorPlan[4][4]);
+	}
+	
 }
